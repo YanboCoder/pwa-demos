a267fb208b921322cb097c4500e101cb5eb8293f
wangyanbo <wangyanbo@huanqiu.com>
2021-03-09 18:00:19 +0800

    modify source path

--- minipwa/public/sw.js
+++ minipwa/public/sw.js
@@ -1,20 +1,20 @@
 // Files to cache
 const cacheName = 'minipwa-v1';
 const appShellFiles = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/style.css',
-  '/favicon.ico',
-  '/images/launch_1125x2436.png',
-  '/icons/icon_48.png',
-  '/icons/icon_72.png',
-  '/icons/icon_96.png',
-  '/icons/icon_120.png',
-  '/icons/icon_144.png',
-  '/icons/icon_180.png',
-  '/icons/icon_192.png',
-  '/icons/icon_512.png',
+  '/pwa-demos/minipwa/public/',
+  '/pwa-demos/minipwa/public/index.html',
+  '/pwa-demos/minipwa/public/app.js',
+  '/pwa-demos/minipwa/public/style.css',
+  '/pwa-demos/minipwa/public/favicon.ico',
+  '/pwa-demos/minipwa/public/images/launch_1125x2436.png',
+  '/pwa-demos/minipwa/public/icons/icon_48.png',
+  '/pwa-demos/minipwa/public/icons/icon_72.png',
+  '/pwa-demos/minipwa/public/icons/icon_96.png',
+  '/pwa-demos/minipwa/public/icons/icon_120.png',
+  '/pwa-demos/minipwa/public/icons/icon_144.png',
+  '/pwa-demos/minipwa/public/icons/icon_180.png',
+  '/pwa-demos/minipwa/public/icons/icon_192.png',
+  '/pwa-demos/minipwa/public/icons/icon_512.png',
 ];
 const contentToCache = appShellFiles;
 
