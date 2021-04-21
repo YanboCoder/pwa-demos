65276c8da99e61752827c1ab6b17705adc7f1386
wangyanbo <wangyanbo@huanqiu.com>
2021-04-21 09:59:57 +0800

    modify the path

--- minipwa/public/app.js
+++ minipwa/public/app.js
@@ -1,11 +1,8 @@
 // Registering Service Worker
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.addEventListener('message', function(event) {
-    console.log('navigator.serviceWorker.onmessage', event);
-  });
+	// navigator.serviceWorker.addEventListener('message', function(event) {
+  //   console.log('navigator.serviceWorker.onmessage', event);
+  // });
 
-	navigator.serviceWorker.register('/pwa-demos/minipwa/public/sw.js').then(function() {
-      return navigator.serviceWorker.ready;
-    })
-    .then(navigator.serviceWorker.controller.postMessage("serviceworker actived!"));
+	navigator.serviceWorker.register('./sw.js');
 }
