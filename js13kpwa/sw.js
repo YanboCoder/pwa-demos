825b6a2b15731c685c91a241b38c26f2a966f5d7
wangyanbo <wangyanbo@huanqiu.com>
2021-06-29 07:39:42 +0800

    modify the fetch event

--- js13kpwa/sw.js
+++ js13kpwa/sw.js
@@ -44,7 +44,7 @@ self.addEventListener('fetch', (e) => {
     const r = await caches.match(e.request);
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
     if (r) return r;
-    const response = await fetch(e.request);
+    const response = await fetch(e.request.url);
     const cache = await caches.open(cacheName);
     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
     cache.put(e.request, response.clone());
