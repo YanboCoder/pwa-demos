e67e4b8073cee29b1e5170cd15e67face4323f5b
wangyanbo <wangyanbo@huanqiu.com>
2021-07-05 18:01:24 +0800

    modify simplepwa

--- simplepwa/app.js
+++ simplepwa/app.js
@@ -1,4 +1,10 @@
 // Registering Service Worker
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/pwa-demos/simplepwa/sw.js');
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker.register('/pwa-demos/simplepwa/sw.js');
+// }
+
+try {
+	window.webkit.messageHandlers.SWWebView.postMessage({})
+} catch (error) {
+	console.log('postMessage error:', error)
 }
